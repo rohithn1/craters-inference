881b26ae30b52e4a2e4ab42af1929b088b4c13c5
rohith nadimpally <rohithnadimpally123@gmail.com>
2025-10-17 02:34:50 -0700

    using publisher script without ros

---
+++ utils/sensors/imu/simple_mpu9250.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import smbus
+import os
+import sys
+import time
+from imusensor.MPU9250 import MPU9250
+import rospy
+from sensor_msgs.msg import Imu
+import numpy as np
+
+def euler_to_quaternion(r, p, y):
+    r = np.deg2rad(r)
+    p = np.deg2rad(p)
+    y = np.deg2rad(y)
+    qx = np.sin(r/2) * np.cos(p/2) * np.cos(y/2) - np.cos(r/2) * np.sin(p/2) * np.sin(y/2)
+    qy = np.cos(r/2) * np.sin(p/2) * np.cos(y/2) + np.sin(r/2) * np.cos(p/2) * np.sin(y/2)
+    qz = np.cos(r/2) * np.cos(p/2) * np.sin(y/2) - np.sin(r/2) * np.sin(p/2) * np.cos(y/2)
+    qw = np.cos(r/2) * np.cos(p/2) * np.cos(y/2) + np.sin(r/2) * np.sin(p/2) * np.sin(y/2)
+    return [qx, qy, qz, qw]
+    
+def talker():
+    address = 0x68
+    bus = smbus.SMBus(1)
+    imu = MPU9250.MPU9250(bus, address)
+    imu.begin()
+    
+    while not rospy.is_shutdown():
+        imu.readSensor()
+        imu.computeOrientation()
+        
+        imu_msg = Imu()
+        imu_msg.header.stamp = rospy.Time.now()
+        imu_msg.header.frame_id = "imu_link"
+        
+        orientation_quaternion = euler_to_quaternion(imu.roll, imu.pitch, imu.yaw)
+        
+        imu_msg.orientation.x = orientation_quaternion[0]
+        imu_msg.orientation.y = orientation_quaternion[1]
+        imu_msg.orientation.z = orientation_quaternion[2]
+        imu_msg.orientation.w = orientation_quaternion[3]
+        imu_msg.orientation_covariance[0] = -1
+        
+        imu_msg.angular_velocity.x = imu.GyroVals[0]
+        imu_msg.angular_velocity.y = imu.GyroVals[1]
+        imu_msg.angular_velocity.z = imu.GyroVals[2]
+        imu_msg.angular_velocity_covariance[0] = -1
+        
+        imu_msg.linear_acceleration.x = imu.AccelVals[0]
+        imu_msg.linear_acceleration.y = imu.AccelVals[1]
+        imu_msg.linear_acceleration.z = imu.AccelVals[2]
+        imu_msg.linear_acceleration_covariance[0] = -1
+
+		print(f"Orientation (deg) - Roll: {imu.roll:.2f}, Pitch: {imu.pitch:.2f}, Yaw: {imu.yaw:.2f}")
+		print(
+			f"Angular Velocity - x: {imu.GyroVals[0]:.3f}, y: {imu.GyroVals[1]:.3f}, z: {imu.GyroVals[2]:.3f}"
+		)
+		print(
+			f"Linear Acceleration - x: {imu.AccelVals[0]:.3f}, y: {imu.AccelVals[1]:.3f}, z: {imu.AccelVals[2]:.3f}"
+		)
