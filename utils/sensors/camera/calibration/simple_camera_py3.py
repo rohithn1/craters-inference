5e1441a5747ce9a0294bb9108856e08c9a0c0e3f
Rohith Nadimpally <rohithnadimpally123@gmail.com>
2025-10-29 00:24:30 -0700

    camera calibration pictures + capture scirpt

---
+++ utils/sensors/camera/calibration/simple_camera_py3.py
@@ -0,0 +1,66 @@
+import cv2
+import datetime
+import os
+
+folder = str(datetime.date.today())
+os.makedirs(folder, exist_ok=True)
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+def show_camera():
+    counter = 0
+    window_title = "CSI Camera"
+    print(gstreamer_pipeline(flip_method=0))
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        try:
+            window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
+            while True:
+                ret_val, frame = video_capture.read()
+                if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(window_title, frame)
+                else:
+                    break
+                keyCode = cv2.waitKey(10) & 0xFF
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+                if keyCode == ord('a'):
+                    path = str("./" + folder + "/img" + str(counter) + ".jpg")
+                    counter += 1
+                    print(f"Saving frame as {path}")
+                    _, frame = video_capture.read()
+                    cv2.imwrite(path, frame)
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Error: Unable to open camera")
+
+if __name__ == "__main__":
+    print("if this script is acting up maybe try moving it to the CSI-Camera directory in /utils??")
+    show_camera()
